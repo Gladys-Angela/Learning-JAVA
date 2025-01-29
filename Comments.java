b683f3874c6f6041f3898d22ca7428d052d546af
Gladys-Angela <gladysangelakamau@gmail.com>
2025-01-29 23:07:07 +0300

    Variables, operators, expressions

---
+++ Comments.java
@@ -0,0 +1,18 @@
+public class Comments {
+    // There are three types of comments in Java 
+    // 1. C++ styles comments -> starts with 2 forward slash "//"
+    //for example... This us a c++ style or single lime comment
+    
+    //2. C-style comments -> starys with "/*" and ends with "*/"
+    // spams multiple lines
+    /*This ia an example of c style or multiline comments
+     
+     */
+    
+    //3. Special Jacadoc comments -> used to generate an HTML documentation for your java programs.
+    /**
+     * This is an example of special java doc comment used for \n generating an HTML documentation. It uses tags like: 
+     * @author Gladys Kamau
+     * @version 1.0.0
+     */
+}
---
+++ OperatorsAndExpressions.java
@@ -0,0 +1,34 @@
+public class OperatorsAndExpressions {
+    public static void main (String[] args){
+    // operations and expressions
+    // Expressions 
+    // ->Expressions are combination of variables, literals and operators that evaluate to a value
+    // Operators are special symbols that perform specific operations on operands (values or variables)
+    
+    //Arithmetic Operartors
+    //Java provides the following arithmetic operators for performing basic mathematical operations:
+
+    int x = 10;
+    int y = 3;
+
+    //1. Addition 
+    int sum = x+y;
+    System.out.println("The sum is: " + sum);
+
+    //subtraction 
+     int difference = x-y;
+     System.out.println("The difference is " + difference);
+     
+     //3Multiplication
+      int multiply = x * y;
+      System.out.println("The multiply is " + multiply);
+
+      //4. Division "/"
+      int division = x / y;
+      System.out.println("The division is " + division);
+      //5 Modulus "%"(Returns the remainder)
+      int modulus = y % x;
+      System.out.println("The modulus is " + modulus);
+
+    }
+}
